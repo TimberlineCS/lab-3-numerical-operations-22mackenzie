21f6c1ab9be39692b51974098671b9a0d62599b6
22mackenzie <22mackenzie.butler@boiseschools.net>
2020-09-08 04:52:58 +0000

    Finished the challenges

--- numbers.java
+++ numbers.java
@@ -5,6 +5,8 @@ Computer science
 public class numbers{
  public static void main (String args[]){
    //challenge 1
+   System.out.println("");
+   System.out.println("challenge 1");
    double d1 = 37.9;
 double d2 = 1004.128;
 int i1 = 12;
@@ -19,29 +21,49 @@ System.out.println(15 - i1* (d1*3) + 4);
 System.out.println(15 - i1*((int)d1*3) + 4	);
 
 //challenge 2
-String number1 = "1234";
-String number1a = number1.substring(0,1);
-String number1b = number1.substring(1,2);
-String number1c = number1.substring(2,3);
-String number1d = number1.substring(3,4);
-String final1= number1d+number1c+number1b+number1a;
-System.out.println(final1);
+System.out.println("");
+System.out.println("challenge 2");
+int number1 = 1234;
 
-String number2 = "6789";
-String number2a = number2.substring(0,1);
-String number2b = number2.substring(1,2);
-String number2c = number2.substring(2,3);
-String number2d = number2.substring(3,4);
-String final2= number2d+number2c+number2b+number2a;
-System.out.println(final2);
+ int number1a= number1%10;
+ int main1 = number1/10;
+ int number1b = main1%10;
+ int main2 = number1/100;
+ int number1c = main2%10;
+ int main3 = number1/1000;
+ System.out.print(number1a);
+System.out.print(number1b);
+System.out.print(number1c);
+System.out.println(main3);
 
-String number3 = "1080";
-String number3a = number3.substring(0,1);
-String number3b = number3.substring(1,2);
-String number3c = number3.substring(2,3);
-String number3d = number3.substring(3,4);
-String final3= number3d+number3c+number3b+number3a;
-System.out.println(final3);
+
+int number2 = 6789;
+
+ int number2a= number2%10;
+ int main1a = number2/10;
+ int number2b = main1a%10;
+ int main2a = number2/100;
+ int number2c = main2a%10;
+ int main3a = number2/1000;
+
+ System.out.print(number2a);
+ System.out.print(number2b);
+ System.out.print(number2c);
+  System.out.println(main3a);
+
+
+int number3 = 1080;
+
+ int number3a= number3%10;
+ int main1b = number3/10;
+ int number3b = main1b%10;
+ int main2b = number3/100;
+ int number3c = main2b%10;
+ int main3b = number3/1000;
+System.out.print(number3a);
+System.out.print(number3b);
+System.out.print(number3c);
+System.out.print(main3b);
 
  }
 }
